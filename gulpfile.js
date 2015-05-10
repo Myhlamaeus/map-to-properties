b294f77bb3b0f49cd4c79db12e56d157f2aa69c2
Malte-Maurice Dreyer <dreyer.maltem+dev@gmail.com>
2015-05-10 12:52:16 +0000

    Use standard & gulp

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp')
+var babel = require('gulp-babel')
+var sourcemaps = require('gulp-sourcemaps')
+var replace = require('gulp-replace')
+var path = require('path')
+var main = path.basename(require('./package.json').main)
+require('babel/register')
+
+gulp.task('build', function () {
+  return gulp.src('{' + main + ',cli.js,lib/**/*.js}')
+    .pipe(sourcemaps.init())
+      .pipe(babel())
+      .pipe(replace(/#!\/usr\/bin\/env babel-node/, '#!/usr/bin/env node'))
+    .pipe(sourcemaps.write('../maps'))
+    .pipe(gulp.dest('dist'))
+})
--- map-to-properties.js
+++ map-to-properties.js
@@ -1,11 +1,11 @@
-export default function mapToProperties(array, object) {
-    if(!Array.isArray(array)) {
-        throw new TypeError("mapToProperties: expects argument 0 to be an array");
-    }
+export default function mapToProperties (array, object) {
+  if (!Array.isArray(array)) {
+    throw new TypeError('mapToProperties: expects argument 0 to be an array')
+  }
 
-    if(typeof(object) !== "object") {
-        throw new TypeError("mapToProperties: expects argument 1 to be an object");
-    }
+  if (typeof object !== 'object') {
+    throw new TypeError('mapToProperties: expects argument 1 to be an object')
+  }
 
-    return array.map((prop) => object[prop]);
+  return array.map((prop) => object[prop])
 }
